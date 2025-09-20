c00ee54d96d2254f7bfc36b6163406ad1acc207c
Tsega Ephrem <tsegaephrem080@gmail.com>
2025-09-20 14:42:53 +0300

    edited serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,36 +3,49 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
             "user_id",
             "first_name",
             "last_name",
+            "full_name",
             "email",
             "phone_number",
             "role",
             "created_at",
         ]
 
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)
+    sender_name = serializers.CharField(source="sender.full_name", read_only=True)
 
     class Meta:
         model = Message
         fields = [
             "message_id",
             "sender",
+            "sender_name",
             "conversation",
             "message_body",
             "sent_at",
         ]
 
+    def validate_message_body(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Message body cannot be empty")
+        return value
+
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    num_messages = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
@@ -40,5 +53,9 @@ class ConversationSerializer(serializers.ModelSerializer):
             "conversation_id",
             "participants",
             "messages",
+            "num_messages",
             "created_at",
         ]
+
+    def get_num_messages(self, obj):
+        return obj.messages.count()
